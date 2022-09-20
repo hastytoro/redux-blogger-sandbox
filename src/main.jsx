50691d0191d9c03edb67642152ac7286b6422ebf
Luigi Lupini <luigi.lupini@gmail.com>
2022-09-20 12:01:27 +0200

    1) Initial setup with temp "dummy" Redux reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={createStore(reducers)}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
